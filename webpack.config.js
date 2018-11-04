38524ce35bd812919e45b1208d8e00aba494e2d1
Valentino Conti <41705567+valentinoConti@users.noreply.github.com>
2018-11-04 15:48:11 -0300

    Lets see

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: `${__dirname}/src/index.js`,
-  output: {
-    path: `${__dirname}/build`,
-    publicPath: '/build/',
-    filename: 'bundle.js',
-  },
+	entry: `${__dirname}/src/index.js`,
+	output: {
+		path: `${__dirname}/build`,
+		publicPath: '/build/',
+		filename: 'bundle.js',
+	},
 
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-    ],
-  },
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+			{ test: /\.html$/, loader: 'html-loader' },
+		],
+	},
 
-  plugins: process.argv.indexOf('-p') === -1 ? [] : [
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false,
-      },
-    }),
-  ],
+	plugins: process.argv.indexOf('-p') === -1 ? [] : [
+		new webpack.optimize.UglifyJsPlugin({
+			output: {
+				comments: false,
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: "./index.html",
+			filename: "./index.html"
+		}),
+	],
 };
