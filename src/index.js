dc2c5e61dfe46df67d37c39b6500ed2c9ecaad48
Valentino Conti <41705567+valentinoConti@users.noreply.github.com>
2018-11-04 16:07:02 -0300

    Updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import Login from './components/loginView/login';
 
 ReactDOM.render(
   <BrowserRouter basename="/valentify/">
-    <App />
+    <Login />
   </BrowserRouter>,
   document.getElementById('root'),
 );
