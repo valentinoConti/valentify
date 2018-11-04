0bba7c6d3c73e5f45dc929cbd1bbcf5e0b6c6c82
Valentino Conti <41705567+valentinoConti@users.noreply.github.com>
2018-11-04 17:45:34 -0300

    urlLoaderAdded

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
 		rules: [
 			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
 			{ test: /\.html$/, loader: 'html-loader' },
+			{ test: /\.(pdf|jpg|png|gif|svg|ico)$/, loader: 'url-loader' },
 		],
 	},
 
